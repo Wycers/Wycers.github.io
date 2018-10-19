22c148022b7d116b188d5ad56fa72ba1a9967b55
wycer <734849165@qq.com>
2018-10-19 15:37:12 +0800

    增加图标

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,13 +3,16 @@ module.exports = {
     title: '记录集',
     description: '记录生活，记录成长',
     base: '/',
+    head: [
+        ['link', { rel: 'icon', href: `/favicon.png` }]
+    ],
     host: '0.0.0.0',
     //mac下port未生效
     port: 8081,
 
     themeConfig: {
         //gitc 仓库地址
-        repo: 'xx/xx',
+        repo: 'Wycers/Wycers.github.io',
         //导航栏
         nav: [
             { text: 'Home', link: '/' }
